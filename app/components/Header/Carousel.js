5804edaaa29f30f6c4b9c21de69c52f0652e28c5
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-11-01 09:21:23 -0700

    UPDATE: the imaga carousel

--- app/components/Header/Carousel.js
+++ app/components/Header/Carousel.js
@@ -14,7 +14,7 @@ export default function Carousel() {
                     loading="eager"
                 />
             ))} */}
-            
+
             {Carouselitems.map(({ link, alt }) => (
                 <Image
                     src={link}
