2994e480eaf2960432076a060510b44adc055a0f
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-11-02 11:51:23 -0700

    FIX: Carousel component because the main Div have 2 div in the same tag Div Div> so we remove one of these

--- app/components/Header/Carousel.js
+++ app/components/Header/Carousel.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import Carouselitem from "./Carouselitem";
 import { Carouselitems } from "../../Data/Carouselitems";
 
 export default function Carousel() {
     return (
-        <div div className="whitespace-nowrap flex flex-row overflow-x-scroll" >
-
+        <div className="whitespace-nowrap flex flex-row overflow-x-scroll" >
             {Carouselitems.map(({ link, alt }) => (
                 <Carouselitem
                     link={link}
