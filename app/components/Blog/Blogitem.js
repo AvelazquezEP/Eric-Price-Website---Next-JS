d40e883c5c7914d1155de52f52b2cd411b419d1c
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-11-06 16:42:52 -0800

    FIX: change the  name clip-rule to clipRule

--- app/components/Blog/Blogitem.js
+++ app/components/Blog/Blogitem.js
@@ -46,7 +46,7 @@ export default function test({ date, image, postLink, alt, title, description, p
                         xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                 </Link>
             </div>
--- app/components/FAQ/FaqItem.js
+++ app/components/FAQ/FaqItem.js
@@ -11,7 +11,7 @@ export default function FaqItem() {
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                     &#191;Pueden ayudarme con mi proceso migratorio&#63;
                 </p>
@@ -34,7 +34,7 @@ export default function FaqItem() {
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                     &#191;Me puede pedir algún familiar para hacerme residente&#63;
                 </p>
@@ -51,7 +51,7 @@ export default function FaqItem() {
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                     Fui víctima de un crimen, &#191;puedo aplicar para algún estatus legal&#63;
                 </p>
@@ -68,7 +68,7 @@ export default function FaqItem() {
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                     Soy residente y quisiera aplicar para la ciudadanía, &#191;puedo llevar el trámite con
                     ustedes&#63;
@@ -86,7 +86,7 @@ export default function FaqItem() {
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                     &#191;Cómo sé si puedo calificar para un asilo&#63;
                 </p>
@@ -103,7 +103,7 @@ export default function FaqItem() {
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd"></path>
+                            clipRule="evenodd"></path>
                     </svg>
                     Perdí mi tarjeta de residencia, &#191;ustedes me pueden ayudar a conseguirla otra vez&#63;
                 </p>
