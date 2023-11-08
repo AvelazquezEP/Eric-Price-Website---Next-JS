0e0545b926cbb63dd059c1d69b8367efa4e4bc6d
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-11-08 11:36:38 -0800

    UPDATE: the Office secntion in the footer, now we have a new structure to can show all the office, the map now will be create in the Office file and not in the itemOffice file

--- app/components/Footer/Itemoffice.js
+++ app/components/Footer/Itemoffice.js
@@ -3,24 +3,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocation } from "@fortawesome/free-solid-svg-icons";
 import { itemOffice } from "../../Data/menuOffice";
 
-export default function Itemoffice() {
+export default function Itemoffice({ link, name, description }) {
     return (
-        <div className="grid grid-cols-2 gap-4 md:mx-auto"> {/* md:w-4/12 */}
-            {itemOffice.map(({ link, name, description }) =>
-                <div className="flex items-center">
-                    <FontAwesomeIcon
-                        icon={faLocation}
-                        className="text-orange-400 text-2xl"
-                    />
+        // <div className="grid grid-cols-2 gap-4 md:mx-auto"> {/* md:w-4/12 */}
+        //     {itemOffice.map(({ link, name, description }) =>
+        <div className="flex items-center">
+            <FontAwesomeIcon
+                icon={faLocation}
+                className="text-orange-400 text-2xl"
+            />
 
-                    <Link href={link} target="_blank">
-                        <div className="ml-2">
-                            <h2 className="font-semibold">{name}</h2>
-                            <p>{description}</p>
-                        </div>
-                    </Link>
+            <Link href={link} target="_blank">
+                <div className="ml-2">
+                    <h2 className="font-semibold">{name}</h2>
+                    <p>{description}</p>
                 </div>
-            )}
+            </Link>
         </div>
+        //     )}
+        // </div>
     );
 }
--- app/components/Footer/Office.js
+++ app/components/Footer/Office.js
@@ -1,11 +1,17 @@
 // import Link from "next/link";
 import Itemoffice from "./Itemoffice";
+import { itemOffice } from "../../Data/menuOffice";
 
 export default function Office() {
     return (
         <div className=" md:w-4/12 md:mx-auto">
             <p className="text-center mb-4 text-lg font-semibold uppercase">Oficinas</p>
-            <Itemoffice />
+            {/* <Itemoffice /> */}
+            <div className="grid grid-cols-2 gap-4 md:mx-auto">
+                {itemOffice.map(({ link, name, description }) =>
+                    <Itemoffice link={link} name={name} description={description} />
+                )}
+            </div>
         </div>
     );
 }
