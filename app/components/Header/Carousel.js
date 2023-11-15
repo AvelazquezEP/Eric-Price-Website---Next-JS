f5911cae224c81611c70d3a463173dc88e616249
Avelazquez <avelazquez@abogadoericprice.com>
2023-11-15 11:31:42 -0800

    UPDATE: the new event in the Form, for now the contact form dont work becuase we cant use the script to make a jquery request

--- app/components/Header/Carousel.js
+++ app/components/Header/Carousel.js
@@ -1,14 +1,12 @@
 import Carouselitem from "./Carouselitem";
 import { Carouselitems } from "../../Data/Carouselitems";
+// import Image from "next/image";
 
 export default function Carousel() {
     return (
-        <div className="whitespace-nowrap flex flex-row overflow-x-scroll" >
-            {Carouselitems.map(({ link, alt }) => (
-                <Carouselitem
-                    link={link}
-                    alt={alt}
-                />
+        <div className="whitespace-nowrap flex flex-row overflow-x-scroll">
+            {Carouselitems.map(({ id, link, alt }) => (
+                <Carouselitem link={link} alt={alt} />                
             ))}
         </div>
     );
