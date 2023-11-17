f27abfc1e463ec0259e92ee5cf433826968cd737
Avelazquez <avelazquez@abogadoericprice.com>
2023-11-17 15:52:37 -0800

    REMOVE: the comments in some files because dont need a anymore

--- app/components/Header/Carouselitem.js
+++ app/components/Header/Carouselitem.js
@@ -1,8 +1,5 @@
 import Image from "next/image";
 
-let width_img = '150px';
-let height_img = '190px';
-
 export default function Carouselitem({ link, alt }) {
     return (
         // <>
--- app/components/Header/navbar.js
+++ app/components/Header/navbar.js
@@ -52,7 +52,6 @@ export default function Navbar() {
         <nav className={`${layoutModule.headerBackground} flex filter drop-shadow-md bg-white px-4 py-4 h-20 items-center`}>
             <MobileNav open={open} setOpen={setOpen} />
             <div className="w-3/12 flex items-center">
-                {/* <a className="text-2xl font-semibold" href="/">LOGO</a> */}
                 <Link href='/'>
                     <Image
                         src='/images/EricpriceLogo-white.svg'
@@ -70,7 +69,6 @@ export default function Navbar() {
                 <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
                     setOpen(!open)
                 }}>
-                    {/* hamburger button/icon */}
                     <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""} `} />
                     <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"} `} />
                     <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""} `} />
