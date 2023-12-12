2db08d67d3caaa41650bcdf3469db4d6e8f7d04f
Avelazquez <avelazquez@abogadoericprice.com>
2023-12-12 15:08:15 -0800

    UPDATE: modified the parameters in the script, we need create the function to can send the data from here in js to eric server in php and can get the response in json

--- public/js/script.js
+++ public/js/script.js
@@ -5,39 +5,42 @@ $(document).ready(function () {
     $("#ButtonSend").on("click", function () {
         // $(this).attr("disabled", "disabled");
 
-        // let firstName = document.getElementById("first_name").value;
-        // let lastName = document.getElementById("last_name").value;
-        // let email = document.getElementById("email").value;
-        // let mobile = document.getElementById("mobile").value;
-        // let location = document.getElementById("00N5f00000SB1X0").value;
-        // let message = document.getElementById('message').value;
-        // let language = document.getElementById('00N5f00000SB1Ws').value;
-        // let meetingType = document.getElementById('meetingTypePerson').value;
+        let firstName = document.getElementById("first_name").value;
+        let lastName = document.getElementById("last_name").value;
+        let email = document.getElementById("email").value;
+        let mobile = document.getElementById("mobile").value;
+        let location = document.getElementById("00N5f00000SB1X0").value;
+        let language = document.getElementById('00N5f00000SB1Ws').value;
+        let meetingType = document.getElementById('meetingTypePerson').value;
+        let message = document.getElementById('message').value;
+        let sms = "Yes";
         // let sms = document.getElementById("00N5f00000SB1XU").value;
 
-        // var name_input = name_input_validation(firstName);
-        // var location_input = location_input_validation(location);
-        // var phone_number = location_input_validation(mobile);
-        // var email_validation = email_input_validation(email);
+        var name_input = name_input_validation(firstName);
+        var location_input = location_input_validation(location);
+        var phone_number = location_input_validation(mobile);
+        var email_validation = email_input_validation(email);
 
-        let tfirstName = "lastname test";
-        let tlastName = "lastname test";
-        let temail = 'test@email.com';
-        let tmobile = '6875425968';
-        let tlocation = 'Los Angeles';
-        let tmessage = "test message";
-        let tlanguage = 'Spanish';
-        let tmeetingType = 'Phone';
-        let tsms = "Yes";
-
-        // log(`${firstName}, ${lastName}, ${email}, ${mobile}, ${location}, ${language}, ${meetingType}, ${message}, ${sms}`)
+        // let tfirstName = "lastname test";
+        // let tlastName = "lastname test";
+        // let temail = 'test@email.com';
+        // let tmobile = '6875425968';
+        // let tlocation = 'Los Angeles';
+        // let tmessage = "test message";
+        // let tlanguage = 'Spanish';
+        // let tmeetingType = 'Phone';
+        // let tsms = "Yes";        
 
         // if (name_input == true && location_input == true && phone_number == true && email_validation) {
-        // sendData(firstName, lastName, email, mobile, location, language, meetingType, message, sms);
-        sendData(tfirstName, tlastName, temail, tmobile, tlocation, tmessage, tlanguage, tmeetingType, tsms);
+        //     sendData(firstName, lastName, email, mobile, location, language, meetingType, message, sms);
+
+        //     sendData(tfirstName, tlastName, temail, tmobile, tlocation, tmessage, tlanguage, tmeetingType, tsms);
         // } else {
         //     setTimeout('$("#ButtonSend").removeAttr("disabled)', 3800);
         // }
+
+        sendData(firstName, lastName, email, mobile, location, language, meetingType, message, sms);
+
     });
 });
 
@@ -49,57 +52,42 @@ const sendData = (firstName, lastName, email, mobile, location, language, meetin
     // document.getElementById('ButtonSend').style.backgroundColor = 'gray';
     // document.getElementById('ButtonSend').innerHTML = 'Sending Data';
 
-    createLeadApi(firstName, lastName, email, mobile, location, language, meetingType, comment, sms);
+    createLeadApi(firstName, lastName, email, mobile, location, language, meetingType, sms, comment);
 }
 
 const createLeadApi = (firstName, lastName, email, mobile, location, language, sms, comment = "-") => {
-    let tfirstName = "lastname test";
-    let tlastName = "lastname test";
-    let temail = 'test@email.com';
-    let tmobile = '6875425968';
-    let tlocation = 'Los Angeles';
-    let tmessage = "test message";
-    let tlanguage = 'Spanish';
-    let tmeetingType = 'Phone';
-    let tsms = "Yes";
-
-    firstName = tfirstName;
-    lastName = tlastName;
-    email = temail;
-    mobile = tmobile;
-    location = tlocation;
-    language = "EP-CA-Website";
-    sms = tsms;
-    comment = tmessage;
 
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         // url: 'https://abogadoericprice.com/apiData.php',
         url: 'https://abogadoericprice.com/test.php',
-        // data: {
-        //     "FirstName": firstName,
-        //     "LastName": lastName,
-        //     "Email": email,
-        //     "LeadSource": "EP-CA-Website",
-        //     "MobilePhone": mobile,
-        //     "Location__c": location,
-        //     "Language__c": language,
-        //     "SMS_Opt_In__c": sms,
-        //     "comments": comment
-        // },        
+        data: {
+            "FirstName": firstName,
+            "LastName": lastName,
+            "Email": email,
+            "LeadSource": "EP-CA-Website",
+            "MobilePhone": mobile,
+            "Location__c": location,
+            "Language__c": language,
+            "SMS_Opt_In__c": sms,
+            "comments": comment
+        },
+        // dataType: 'json',
         dataType: 'json',
         success: function (data) {
-            console.log(data);
 
-            // var fullUrl = "";
-            // let leadID = data.id;
+            var fullUrl = "";
+            let leadID = data.id;
 
-            // let locationCode = getLocation(location); 
+            log(data);
+
+            let locationCode = getLocation(location);
+            log(locationCode);
 
             // let inPerson = "OUR_LOCATION";
             // let byPhone = "VID_CONFERENCE";
 
-            // sendEmail(firstName, lastName, email, mobilePhone, language, leadID, comment);
+            // // sendEmail(firstName, lastName, email, mobilePhone, language, leadID, comment);
 
             // if (leadID == "" || leadID == null || leadID == undefined) {
             //     let url_thanks = '/Thanks';
