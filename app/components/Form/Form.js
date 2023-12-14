51ca3c6e27850fe80a552bda763c0e3fab054813
Avelazquez <avelazquez@abogadoericprice.com>
2023-12-13 17:07:12 -0800

    UPDATE: the label/description

--- app/components/Form/Form.js
+++ app/components/Form/Form.js
@@ -48,7 +48,7 @@ export default function Form() {
 
                             <div>
                                 <label htmlFor="mobile" className="block mb-2 text-sm font-medium text-gray-900">
-                                    Teléfono Movil
+                                    Teléfono Moviil
                                 </label>
                                 <input id="mobile" type="text" name="mobile" maxLength={10} size={20}
                                     className="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
